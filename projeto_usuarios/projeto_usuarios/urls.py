7b73eb204dd4734ee3234a9f47a87e28f9695825
Pedro Henrique <pedrobezerraout@gmail.com>
2024-06-05 10:27:22 -0300

    ProjetoDjango Finalizado
    Projeto aprendizado finalizado com cadastro de usuários em banco de dados

--- projeto_usuarios/projeto_usuarios/urls.py
+++ projeto_usuarios/projeto_usuarios/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     #rota, view responsável, nome de referência
     #usuarios.com
     path('', views.home, name='home'),
+    #usuarios.com/usuarios
+    path('usuarios/', views.usuarios,name='listagem_usuarios'),
 ]
